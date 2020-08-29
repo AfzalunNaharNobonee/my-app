8d683fbf4c72417e5ac6a66a3ce0284fcf39cc89
AfzalunNaharNobonee <afzalun2nobonee@gmail.com>
2020-08-28 23:55:56 -0700

    done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
         <header>
             <img src={logo} alt="logo" />
             <hr/>
-            <p>We are helping you to learn something that you like. You will have access to these courses for life-time. Well-known professors will help you to learn more about your selected courses and you surely will not regret. ENJOY!!! </p>
+            <p>We are helping you to learn something that you like. You will have access to these courses for life-time. Well-known professors will help you to learn more about your selected courses and you surely will not regret. So just enroll and do the courses. It will make you happy. The courses are easily explained and you will be given very small tasks so that you can check your pregess. Have fun. ENJOY!!! </p>
             
         </header>
     );
